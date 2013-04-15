7d4373a928ecc8d9fe9cc1e5d69c73db45b12f8f
Rodrigo Dumont <rlsdumont@gmail.com>
2013-04-15 13:36:09 -0300

    Finishes NuGet packaging

---
+++ src/Runtime/content/readme.txt
@@ -0,0 +1,4 @@
+NuGet Content Generator
+
+A reference to NuGetContentGenerator.targets has beend added to your project.
+Before every build, all "*.cs" files inside the "content" folder will be transfomed into "*.cs.pp".
---
+++ src/Runtime/tools/uninstall.ps1
@@ -0,0 +1,26 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$packageId = $package.Id
+$packageVersion = $package.Version
+$packageFolder = "$packageId.$packageVersion"
+$toolsDestPath = "$installPath\..\..\tools"
+$toolsDestPath = [System.IO.Path]::GetFullPath($toolsDestPath)
+
+$targetsFile = "TransformNuGetContent.targets"
+$assemblyFile = "NuGetContentGenerator.dll"
+
+[System.IO.File]::Delete("$toolsDestPath\$targetsFile")
+[System.IO.File]::Delete("$toolsDestPath\$assemblyFile")
+
+$buildProject = Get-MSBuildProject
+
+$existingImports = $buildProject.Xml.Imports |
+	Where-Object { $_.Project -like "*\$targetsFile" }
+	
+if ($existingImports) {
+	$existingImports |
+		ForEach-Object {
+			$buildProject.Xml.RemoveChild($_) | Out-Null
+		}
+	$project.Save()
+}
